28e4982bf82b02589328405d231b37214160499a
Duke099 <68139285+Duke099@users.noreply.github.com>
2022-03-10 04:15:19 +0530

    Add files via upload

---
+++ Lecture_Programmes/Lecture_1-16/A31_SmallestLetter.java
@@ -0,0 +1,32 @@
+package com.company;
+
+//Q: Find Smallest character greater than a target.
+public class A31_SmallestLetter {
+    public static void main(String[] args) {
+        char[] letters = {'a', 'd', 'f', 'h'};
+        char target = 'h';
+        char ans = letters(letters, target);
+        System.out.println(ans);
+
+    }
+    static char letters(char[] letters, char target) {
+
+        if (target == letters[letters.length - 1]) {
+            return letters[0];
+        }
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) /2;
+            if (target <  letters[mid]) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length]  ;
+    }
+}
